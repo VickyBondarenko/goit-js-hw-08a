9e5892e9d10924b0b4668faeecc6ce990df5e83a
VickyBondarenko <quikyv@gmail.com>
2023-01-31 21:07:43 +0100

    Add task2,3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,32 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-const player = new Player('handstick', {
-  id: 19231868,
-  width: 640,
-});
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+// const throttle = require('lodash.throttle');
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+player.on(
+  'timeupdate',
+  throttle(function (e) {
+    // console.log(e.seconds);
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(e.seconds));
+  }, 1000)
+);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+const parsedSavedTime = JSON.parse(savedTime);
+console.log(parsedSavedTime);
+player
+  .setCurrentTime(parsedSavedTime)
+  .then(function (seconds) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(0));
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+// const email = document.querySelector('input');
+// const message = document.querySelector('textarea');
+
+const { email, message } = form;
+console.log(form);
+
+form.addEventListener('input', throttle(inputHandler), 500);
+function inputHandler(event) {
+  //   event.preventDefault();
+
+  const formData = {
+    email: email.value,
+    message: message.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  event.target.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function getDataFromStorage() {
+  try {
+    const qurentData = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if (qurentData !== null) {
+      email.value = qurentData.email;
+      message.value = qurentData.message;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getDataFromStorage();
